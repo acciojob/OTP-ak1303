4ea6037c8b45387652d3cb942b106c9e3485918f
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 19:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.addEventListener('click', (e) => {
+  if (e.target.tagName === 'INPUT') {
+    e.target.value = '';
+  }
+});
+document.addEventListener('DOMContentLoaded',()=>{
+	let inputs = document.querySelectorAll('.code-container input');
+	inputs.forEach((input,index)=>{
+		input.setAttribute('maxLength',1);
+		input.addEventListener('input',()=>{
+			if(input.value.trim()){
+				if(index+1<inputs.length){
+					inputs[index+1].focus();
+					inputs[index+1].value='';
+				}
+			}
+		});
+	input.addEventListener('keydown', (e) => {
+      if (e.key === 'Backspace' && index > 0) {
+        // Handle Backspace key: move focus to the previous input
+		inputs[index].value='';
+        inputs[index - 1].focus();
+      }
+    });
+	});
+});
