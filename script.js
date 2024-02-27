87239ef058f7d56e2736b71a8407bfda5ba063f2
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-27 20:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ document.addEventListener('click', (e) => {
     e.target.value = '';
   }
 });
-document.addEventListener('DOMContentLoaded',()=>{
-	let inputs = document.querySelectorAll('.code-container input');
-	inputs.forEach((input,index)=>{
-		input.setAttribute('maxLength',1);
-		input.addEventListener('input',()=>{
-			if(input.value.trim()){
-				if(index+1<inputs.length){
-					inputs[index+1].focus();
-					inputs[index+1].value='';
-				}
-			}
-		});
-	input.addEventListener('keydown', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
+  let inputs = document.querySelectorAll('.code-container input');
+
+  inputs.forEach((input, index) => {
+    input.setAttribute('maxLength', 1);
+
+    input.addEventListener('input', () => {
+      if (input.value.trim()) {
+        if (index + 1 < inputs.length) {
+          inputs[index + 1].focus();
+          inputs[index + 1].value = '';
+        }
+      }
+    });
+
+    input.addEventListener('keydown', (e) => {
       if (e.key === 'Backspace' && index > 0) {
         // Handle Backspace key: move focus to the previous input
-		inputs[index].value='';
+        inputs[index].value = '';
         inputs[index - 1].focus();
       }
     });
-	});
+  });
 });
